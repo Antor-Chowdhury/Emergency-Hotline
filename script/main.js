7e9f992964651db2de184a0a30f7815e3602f479
Antor-Chowdhury <antor1347@gmail.com>
2025-08-28 22:00:47 +0600

    add call buttons functionality

--- script/main.js
+++ script/main.js
@@ -15,3 +15,49 @@ for (const heart of hearts) {
     heartIconCounter.innerText = updatedHeartCount;
   });
 }
+
+// ----- Call Button Functionality -----
+
+const callButtons = document.getElementsByClassName("btn-call");
+
+const serviceNames = document.getElementsByClassName("service-name");
+
+const serviceNumbers = document.getElementsByClassName("service-number");
+
+const totalCoins = document.getElementById("coin");
+
+const callHistory = document.getElementById("call-history-container");
+
+for (let i = 0; i < callButtons.length; i++) {
+  callButtons[i].addEventListener("click", function () {
+    const serviceName = serviceNames[i].innerText;
+    const serviceNumber = serviceNumbers[i].innerText;
+
+    let coin = parseInt(totalCoins.innerText);
+    if (coin > 20) {
+      alert(`☎️ Calling ${serviceName} ${serviceNumber}...`);
+      coin -= 20;
+
+      const time = new Date().toLocaleTimeString();
+
+      const div = document.createElement("div");
+      div.innerHTML = `
+            <div
+              class="flex justify-between items-center p-4 bg-gray-100 mt-4 rounded-lg"
+            >
+              <div>
+                <h2>${serviceName}</h2>
+                <p>${serviceNumber}</p>
+              </div>
+              <div>
+                <p>${time}</p>
+              </div>
+            </div>`;
+
+      callHistory.appendChild(div);
+    } else {
+      alert("Not enough coin");
+    }
+    totalCoins.innerText = coin;
+  });
+}
